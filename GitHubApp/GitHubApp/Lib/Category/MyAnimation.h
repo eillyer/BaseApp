c6bd2ac4ba785a6b920371d4d393b6afc79b0a3b
eillyer <475124896@qq.com>
2019-02-15 18:11:24 +0800

    新增动画特效

--- GitHubApp/GitHubApp/Lib/Category/MyAnimation.h
+++ GitHubApp/GitHubApp/Lib/Category/MyAnimation.h
@@ -8,18 +8,57 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^animationFinisnBlock)(BOOL finish);
+
 @interface MyAnimation : NSObject
+
+@property (strong , nonatomic) CALayer *layer;
+@property (copy , nonatomic) animationFinisnBlock animationFinisnBlock;
 
 
 /**
- 控件掉落动画<必须将这个类定义成属性，持有这个类，否则，动画无效果>
-使用时需要将此类初始化为属性变量
+ 单例
  
- @property (nonatomic, strong) MyAnimation *animation;
- self.animation = [[MyAnimation alloc] init];
+ @return self
+ */
++ (instancetype)shareTool;
 
+/**
+ 
+ 控件掉落动画<必须将这个类定义成属性，持有这个类，否则，动画无效果>
+ 
  @param arr 掉落的控件集合
  @param inView 在父view
  */
 - (void)animationWithDiaoLuo:(NSArray <UIView*>*)arr inView:(UIView *)inView;//掉落效果，将view加到arr中
+
+/**
+ *  开始转走动画
+ *
+ *  @param view        添加动画的view
+ *  @param rect        view 的绝对frame
+ *  @param finishPoint 下落的位置
+ *  @param completion 动画完成回调
+ 
+ 
+ 使用方法
+ BaseGoogsItem *cell = (BaseGoogsItem *)[collectionView cellForItemAtIndexPath:indexPath];
+ UIImageView *imageView = (UIImageView *)[cell valueForKey:@"goodsImage"];
+ CGRect rect = cell.frame;
+ /// 获取当前cell 相对于self.view 当前的坐标
+ rect.origin.y          = rect.origin.y - collectionView.contentOffset.y;
+ CGRect imageViewRect   = imageView.frame;
+ imageViewRect.origin.x = rect.origin.x;
+ imageViewRect.origin.y = rect.origin.y + imageViewRect.origin.y;
+ [[MyAnimation shareTool] animationZhuanZouView:imageView rect:imageViewRect finisnPoint:CGPointMake(0,0) finishBlock:^(BOOL finish) {
+ [self.view worningShake];
+ }];
+ 
+ 
+ 
+ */
+- (void)animationZhuanZouView:(UIView *)view
+                         rect:(CGRect)rect
+                  finisnPoint:(CGPoint)finishPoint
+                  finishBlock:(animationFinisnBlock)completion;
 @end
--- GitHubApp/GitHubApp/Lib/Category/MyAnimation.m
+++ GitHubApp/GitHubApp/Lib/Category/MyAnimation.m
@@ -8,7 +8,7 @@
 
 #import "MyAnimation.h"
 
-@interface MyAnimation ()
+@interface MyAnimation ()<CAAnimationDelegate>
 @property (nonatomic, strong) UIGravityBehavior *gravityBeahvior;
 @property (nonatomic, strong) UIGravityBehavior *gravity;
 @property (nonatomic, strong) UICollisionBehavior *collision;
@@ -19,6 +19,14 @@
 
 @implementation MyAnimation
 
++ (instancetype)shareTool{
+    static MyAnimation *helper = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        helper = [[MyAnimation alloc] init];
+    });
+    return helper;
+}
 - (void)animationWithDiaoLuo:(NSArray <UIView*>*)arr inView:(UIView *)inView{
     UIDynamicAnimator *animator = [[UIDynamicAnimator alloc]initWithReferenceView:inView];
     _animator = animator;
@@ -41,4 +49,77 @@
     [self.animator addBehavior:dynamicItemBehavior];
 }
 
+
+
+#pragma public function
+- (void)animationZhuanZouView:(UIView *)view
+                         rect:(CGRect)rect
+                  finisnPoint:(CGPoint)finishPoint
+                  finishBlock:(animationFinisnBlock)completion
+{
+    //图层
+    _layer = [CALayer layer];
+    _layer.contents = view.layer.contents;
+    _layer.contentsGravity = kCAGravityResizeAspectFill;
+    rect.size.width  = rect.size.width;
+    rect.size.height = rect.size.height;   //重置图层尺寸
+    _layer.bounds = rect;
+    //    _layer.cornerRadius  = rect.size.width/2;
+    //    _layer.masksToBounds = YES;          //圆角
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    [keyWindow.layer addSublayer:_layer];
+    _layer.position = CGPointMake(rect.origin.x+view.frame.size.width/2, CGRectGetMidY(rect)); //a 点
+    /// 路径
+    [self createAnimationwithRect:rect finishPoint:finishPoint];
+    /// 回调
+    if (completion) {
+        _animationFinisnBlock = completion;
+    }
+}
+
+#pragma mark - private function
+/// 创建动画
+- (void)createAnimationwithRect:(CGRect)rect
+                    finishPoint:(CGPoint)finishPoint {
+    /// 路径动画
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:_layer.position];
+    [path addQuadCurveToPoint:finishPoint controlPoint:CGPointMake(kScreenWidth/2, rect.origin.y-80)];
+    CAKeyframeAnimation *pathAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    pathAnimation.path = path.CGPath;
+    /// 旋转动画
+    CABasicAnimation *rotateAnimation   = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotateAnimation.removedOnCompletion = YES;
+    rotateAnimation.fromValue = [NSNumber numberWithFloat:0];
+    rotateAnimation.toValue   = [NSNumber numberWithFloat:12];
+    rotateAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    /// 缩放动画
+    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation.removedOnCompletion = YES;
+    scaleAnimation.fromValue = [NSNumber numberWithFloat:1.0]; // 开始时的倍率
+    scaleAnimation.toValue = [NSNumber numberWithFloat:0.25]; // 结束时的倍率
+    scaleAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    /// 添加动画动画组合
+    CAAnimationGroup *groups = [CAAnimationGroup animation];
+    groups.animations = @[pathAnimation,rotateAnimation,scaleAnimation];
+    groups.duration = 1.0f;
+    groups.removedOnCompletion = NO;
+    groups.fillMode = kCAFillModeForwards;
+    groups.delegate = self;
+    [_layer addAnimation:groups forKey:@"group"];
+}
+
+#pragma mark - CAAnimationDelegate
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    if (anim == [_layer animationForKey:@"group"]) {
+        [_layer removeFromSuperlayer];
+        _layer = nil;
+        if (_animationFinisnBlock) {
+            _animationFinisnBlock(YES);
+        }
+    }
+}
 @end
