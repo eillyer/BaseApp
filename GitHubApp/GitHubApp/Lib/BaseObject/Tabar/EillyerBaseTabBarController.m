cd0be64020e03bea7df1fe3b02debd63dd17372a
eillyer <475124896@qq.com>
2019-04-18 18:18:31 +0800

    tabbar点击动画

--- GitHubApp/GitHubApp/Lib/BaseObject/Tabar/EillyerBaseTabBarController.m
+++ GitHubApp/GitHubApp/Lib/BaseObject/Tabar/EillyerBaseTabBarController.m
@@ -22,6 +22,10 @@
 
 @interface EillyerBaseTabBarController ()
 
+//记录上一次点击tabbar，使用时，记得先在init或viewDidLoad里 初始化 = 0
+
+@property (nonatomic,assign) NSInteger  indexFlag;
+
 @end
 
 @implementation EillyerBaseTabBarController
@@ -80,6 +84,35 @@
     //        }
     //    }
     return YES;
+}
+
+
+//点击动画效果
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    NSInteger index = [self.tabBar.items indexOfObject:item];
+    if (index != self.indexFlag) {
+        //执行动画
+        NSMutableArray *arry = [NSMutableArray array];
+        for (UIView *btn in self.tabBar.subviews) {
+            if ([btn isKindOfClass:NSClassFromString(@"UITabBarButton")]) {
+                [arry addObject:btn];
+            }
+        }
+        //添加动画
+        
+        //放大效果，并回到原位
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        //速度控制函数，控制动画运行的节奏
+        animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        animation.duration = 0.2;       //执行时间
+        animation.repeatCount = 1;      //执行次数
+        animation.autoreverses = YES;    //完成动画后会回到执行动画之前的状态
+        animation.fromValue = [NSNumber numberWithFloat:0.7];   //初始伸缩倍数
+        animation.toValue = [NSNumber numberWithFloat:1.3];     //结束伸缩倍数
+        [[arry[index] layer] addAnimation:animation forKey:nil];
+        
+        self.indexFlag = index;
+    }
 }
 
 
